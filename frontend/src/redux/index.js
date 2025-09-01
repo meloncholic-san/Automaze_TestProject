d22fc8fce220fc1b3d7f110d56f6eae263164376
Panchenko Denis <Pandenisko@gmail.com>
2025-09-01 11:44:07 +0300

    updated baseURL for Production

--- frontend/src/redux/index.js
+++ frontend/src/redux/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { selectToken } from "./auth/selectors";
 
 const isProd = import.meta.env.PROD;
 
 export const api = axios.create({
   baseURL: isProd
-    ? "https://dzencode-testproject.onrender.com/"
+    ? "https://automaze-testproject.onrender.com"
     : "http://localhost:8080/",
   withCredentials: true,
 });
